4a8d841a6a67e84a3949e88074d78d5a2ac12e76
Junwei Liang <junweiliang1114@gmail.com>
2025-03-05 17:48:49 +0800

    updated

--- g1_realrobot/calibrate_intrinsics_depthcam.py
+++ g1_realrobot/calibrate_intrinsics_depthcam.py
@@ -248,6 +248,8 @@ class DepthCamera(object):
         scale = orbbec_depth_frame.get_depth_scale() # scale is 1.0, the HW are in milimeters
 
         print(width)
+        data = orbbec_depth_frame.get_data()
+        print("ok")
         depth_data = np.frombuffer(orbbec_depth_frame.get_data(), dtype=np.uint16)
         print(height)
         depth_data = depth_data.reshape((height, width), order='C')
