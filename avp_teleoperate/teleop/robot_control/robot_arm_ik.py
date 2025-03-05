c309ec78e93770b3347aa6e825b3b485dd4c8c53
Junwei Liang <junweiliang1114@gmail.com>
2025-03-05 12:37:01 +0800

    updated

--- avp_teleoperate/teleop/robot_control/robot_arm_ik.py
+++ avp_teleoperate/teleop/robot_control/robot_arm_ik.py
@@ -545,8 +545,8 @@ if __name__ == "__main__":
     L_start = pin.SE3(pin.Quaternion(1, 0, 0, 0), np.array([0.25, 0.25, 0.1]))
     L_target = pin.SE3(pin.Quaternion(1, 0, 0, 0), np.array([0.4, 0.1, 0.3]))
 
-    arm_ik.vis.set_object(pin.visual.Frame(0.1))
-    arm_ik.vis.set_transform(L_target.homogeneous)
+    arm_ik.vis.viewer["L_ee_target"].set_object(pin.visual.Frame(0.1))
+    arm_ik.vis.viewer["L_ee_target"].set_transform(L_target.homogeneous)
 
     # 这里会以100Hz计算IK，把左手从初始位置移动到一个目标位置，然后回来，丝滑
 
