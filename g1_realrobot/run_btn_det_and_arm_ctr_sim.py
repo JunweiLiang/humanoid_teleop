104e9739b41cf5fc42432bfdf4e95d1c126852e5
Junwei Liang <junweiliang1114@gmail.com>
2025-03-15 22:10:12 +0800

    updated

--- g1_realrobot/run_btn_det_and_arm_ctr_sim.py
+++ g1_realrobot/run_btn_det_and_arm_ctr_sim.py
@@ -401,6 +401,12 @@ def camera_frame_to_robot_frame(xyz_in_camera, T_base_to_camera=None):
             [0, -1,  0,  0.65],
             [0,  0,  0,  1]
         ])
+        T_base_to_camera = np.array([
+            [np.cos(np.radians(-45)),  0, -np.sin(np.radians(-45)),  0.05],
+            [-1,        0,  0,         0],
+            [np.sin(np.radians(-45)), 0,  np.cos(np.radians(-45)),  0.65],
+            [0,         0,  0,         1]
+        ])
         # Convert to homogeneous coordinates
         P_camera_homogeneous = np.append(P_camera, 1)  # [X, Y, Z, 1]
 
