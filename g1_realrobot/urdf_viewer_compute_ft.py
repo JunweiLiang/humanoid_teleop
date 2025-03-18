145e34dd6eb72d5d1b3124be88d1ff0ce14def01
Junwei Liang <junweiliang1114@gmail.com>
2025-03-18 10:20:03 +0800

    updated

--- g1_realrobot/urdf_viewer_compute_ft.py
+++ g1_realrobot/urdf_viewer_compute_ft.py
@@ -10,6 +10,7 @@ import time
 from pinocchio.visualize import MeshcatVisualizer
 import pinocchio as pin
 import meshcat.geometry as g
+import meshcat.geometry as mg
 
 parser = argparse.ArgumentParser()
 parser.add_argument("urdf")
@@ -86,13 +87,40 @@ if __name__ == "__main__":
     vis.viewer["target"].set_transform(target_pose.homogeneous)
     vis.viewer["ee/sphere"].set_object(g.Sphere(0.02), g.MeshLambertMaterial(color=red))
     vis.viewer["ee"].set_transform(ee_pose.homogeneous)
-    vis.displayFrames(True, frame_ids=[ee_frame_id, origin_frame_id, target_frame_id], axis_length = 0.15, axis_width = 5)
+
 
     for frame_name in visualization_list:
         pose = robot.data.oMf[robot.model.getFrameId(frame_name)]
         vis.viewer["%s/sphere" % frame_name].set_object(g.Sphere(0.02), g.MeshLambertMaterial(color=green))
         vis.viewer[frame_name].set_transform(pose.homogeneous)
 
+    # Enable the display of end effector target frames with short axis lengths and greater width.
+    frame_viz_names = [origin_frame, target_frame, "R_ee"] + visualization_list
+    FRAME_AXIS_POSITIONS = (
+        np.array([[0, 0, 0], [1, 0, 0],
+                  [0, 0, 0], [0, 1, 0],
+                  [0, 0, 0], [0, 0, 1]]).astype(np.float32).T
+    )
+    FRAME_AXIS_COLORS = (
+        np.array([[1, 0, 0], [1, 0.6, 0],
+                  [0, 1, 0], [0.6, 1, 0],
+                  [0, 0, 1], [0, 0.6, 1]]).astype(np.float32).T
+    )
+    axis_length = 0.1
+    axis_width = 10
+    for frame_viz_name in frame_viz_names:
+        vis.viewer[frame_viz_name].set_object(
+            mg.LineSegments(
+                mg.PointsGeometry(
+                    position=axis_length * FRAME_AXIS_POSITIONS,
+                    color=FRAME_AXIS_COLORS,
+                ),
+                mg.LineBasicMaterial(
+                    linewidth=axis_width,
+                    vertexColors=True,
+                ),
+            )
+        )
 
 
     while True:
